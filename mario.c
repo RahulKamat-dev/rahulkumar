2b07b09560f7511b0534665b77686675728a7ed2
rahulKmt1998 <rahulKmt1998@users.noreply.github.com>
2020-01-29 03:55:41 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -6,7 +6,7 @@ int main(void)
   int n ;
     do
     {
-        n = get_int("Height : " );
+        n = get_int("Height : ");
     }while(n<1 || n>8);
     for(int i=0;i<n;i++)
     {
@@ -18,7 +18,7 @@ int main(void)
                 printf("#");
         }
         
-         printf("\n");
-     }
+       printf("\n");
+    }
 
 }
