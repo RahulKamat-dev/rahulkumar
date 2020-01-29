36b70541bd0ed3a969ae1ff38a5dd0f59ddff306
rahulkmt1998 <rahulkmt1998@users.noreply.github.com>
2020-01-29 08:04:47 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -11,47 +11,47 @@ int sentence;
 int main(void)
 {
 
-   string alp = get_string("What's the article?: ");
+    string alp = get_string("What's the article?: ");
 
-   int n = strlen(alp);
+    int n = strlen(alp);
 
-   if (isalnum(alp[0]))
-   {
-       word = 1;
-   }
+    if (isalnum(alp[0]))
+    {
+        word = 1;
+    }
 
-   for (int i = 0; i < n;  i++)
-   {
-       if (isalnum(alp[i]))
-       {
-           letter++;
-       }
+    for (int i = 0; i < n;  i++)
+    {
+        if (isalnum(alp[i]))
+        {
+            letter++;
+        }
 
-       if (i < n - 1 && isspace(alp[i]) && isalnum(alp[i + 1]))
-       {
-           word++;
-       }
+        if (i < n - 1 && isspace(alp[i]) && isalnum(alp[i + 1]))
+        {
+            word++;
+        }
 
-       if (i > 0 && (alp[i] == '!' || alp[i] == '?' || alp[i] == '.') && isalnum(alp[i - 1]))
-       {
-           sentence++;
-       }
+        if (i > 0 && (alp[i] == '!' || alp[i] == '?' || alp[i] == '.') && isalnum(alp[i - 1]))
+        {
+            sentence++;
+        }
 
-   }
-   int grade = 0.0588 * (100 * letter / word) - 0.296 * (100 * sentence / word) - 15.8;
+    }
+    int grade = 0.0588 * (100 * letter / word) - 0.296 * (100 * sentence / word) - 15.8;
 
-   printf("Letters: %i\n Words: %i\n Sentences: %i\n", letter, word, sentence);
+    printf("Letters: %i\n Words: %i\n Sentences: %i\n", letter, word, sentence);
 
-   if (grade <= 1)
-   {
-       printf("Before Grade 1\n");
-   }
-   else if (grade < 16)
-   {
-       printf("Grade %i\n", grade);
-   }
-   else
-   {
-       printf("Grade 16+\n");
-   }
+    if (grade <= 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade < 16)
+    {
+        printf("Grade %i\n", grade);
+    }
+    else
+    {
+        printf("Grade 16+\n");
+    }
 }
