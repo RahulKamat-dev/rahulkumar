3a63e63d477142ede976a7ef5411d47ec613d4f4
rahulKmt1998 <rahulKmt1998@users.noreply.github.com>
2020-01-29 05:18:44 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -3,19 +3,24 @@
 
 int main(void)
 {
-  int n;
+    int n;
     do
     {
         n = get_int("Height : ");
-    }while(n<1 || n>8);
-    for(int i = 0;i < n; i++)
+    }
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0;j < 2*n + 2; j++)
+        for (int j = 0; j < 2 * n + 2; j++)
         {
-            if(i+j<n-1 || j==n || j==n+1)
-               printf(" ");
+            if (i +j < n - 1 || j == n || j == n + 1)
+            {
+                printf(" ");
+            }
             else if(i+j>=n-1 && j-i<=n+2)
+            {
                 printf("#");
+            }
          }
         printf("\n");
     }
