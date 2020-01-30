29a929f660a43435fcfdbb055872dc0aa2222751
rahulkmt1998 <rahulkmt1998@users.noreply.github.com>
2020-01-30 04:31:22 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,39 +1,69 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+int main(int argc, string argv[])
 {
+// Validate argv is a single input
     if (argc != 2)
     {
-        printf("Usage: ./caesar k\n");
+        printf("Usage: ./casesar key \n");
         return 1;
     }
-    int k = atoi(argv[1]);
-    if (k < 0)
+    int n = strlen(argv[1]);
+// Validate that argv is a digit
+    for (int i = 0; i < n; i++)
     {
-        printf("key must be positive\n");
-        return 1;
+        if (isdigit(argv[1][i]) == 0)
+        {
+            printf("Usage:./caesar key\n");
+            return 1;
+        }
     }
-    string plaintext = get_string("plaintext: ");
+// Cast argv to a int
+    int key = atoi(argv[1]);
+// Divide so key is a single transfer through alpha
+    if (key >= 26)
+    {
+        key = key % 26;
+    }
+// Get text from user to crypt
+    string ptext = get_string("plaintext: ");
+    int j = strlen(ptext);
     printf("ciphertext: ");
-    for (int i = 0, len = strlen(plaintext); i < len; i++)
+    for (int i = 0; i < j; i++)
     {
-        if (islower(plaintext[i]))
+// Determine whether char is lower alpha
+        if (ptext[i] >= 'a' && ptext[i] <= 'z')
         {
-            printf("%c", (plaintext[i] - 'a' + k) % 26 + 'a');
+// Add the key to the ascii decimal number
+            int cipher = ptext[i] + key;
+            if (cipher > 122)
+            {
+// Wrap the cipher around if it plus key is greater than z
+                cipher = cipher - 26;
+            }
+            printf("%c", cipher);
         }
-        else if (isupper(plaintext[i]))
+// Do the same as above for upper
+        else if (ptext[i] >= 'A' && ptext[i] <= 'Z')
         {
-            printf("%c", (plaintext[i] - 'A' + k) % 26 + 'A');
+            int cipher = ptext[i] + key;
+            if (cipher > 90)
+            {
+                cipher -= 26;
+            }
+            printf("%c", cipher);
         }
+// Preserve all other charcaters as is
         else
         {
-            printf("%c", plaintext[i]);
+            printf("%c", ptext[i]);
         }
     }
     printf("\n");
-
+// Exit the program
+    return 0;
 }
